7c7661fcc3b4e4c96b70e660cefddedd07eb609a
canhnht <canhnht1709@gmail.com>
2021-08-21 18:19:40 +0900

    Display ore names

--- src/components/MineSiteInput/index.jsx
+++ src/components/MineSiteInput/index.jsx
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-import "./styles.scss";
+import './styles.scss';
 
 function MineSiteInput() {
-  const [names, setNames] = useState([""]);
+  const [names, setNames] = useState(['']);
+  const history = useHistory();
 
   const showOres = () => {
-    setNames([...names, ""])
+    setNames([...names, '']);
+  };
 
-  }
+  const showOverview = () => {
+    // Chuyen sang trang /overview
+    // Pass bien names vao trang /overview
+    history.push({
+      pathname: '/overview',
+      state: names,
+    });
+  };
+
+  const handleChangeInput = (evt, index) => {
+    const value = evt.target.value;
+    console.log('handleChangeInput', index, value);
+    // Thay doi phan tu thu index trong mang names
+    setNames([...names.slice(0, index), value, ...names.slice(index + 1)]);
+  };
+
+  // ['a1', 'a2', 'a3']
+  // index = 1, value = 'b2' => ['a1', 'b2', 'a3']
+
   return (
     <div className="mine-site-input">
       Mine Site Input
@@ -18,11 +39,17 @@ function MineSiteInput() {
           <input
             type="text"
             id={`ore${index + 1}`}
+            value={val}
+            onChange={(evt) => handleChangeInput(evt, index)}
           />
         </div>
       ))}
-      <button id="addMoreButton" onClick={showOres}>Add More</button>
-      <button id="doneButton">Done Button</button>
+      <button id="addMoreButton" onClick={showOres}>
+        Add More
+      </button>
+      <button id="doneButton" onClick={showOverview}>
+        Done Button
+      </button>
     </div>
   );
 }
--- src/components/MineSiteOverview/index.jsx
+++ src/components/MineSiteOverview/index.jsx
@@ -1,9 +1,25 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import './styles.scss';
 
 function MineSiteOverview() {
-  return <div className="mine-site-input">MineSiteOverview</div>;
+  const location = useLocation();
+  // Lay state tu trong history.push
+  const { state } = location;
+  console.log({ state });
+  const sortedNames = state.sort();
+  console.log({ sortedNames });
+
+  return (
+    <div className="mine-site-input">
+      <ul>
+        {sortedNames.map((name, idx) => (
+          <li key={idx}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default MineSiteOverview;
